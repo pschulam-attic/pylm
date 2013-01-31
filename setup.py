beb0c7144c6e854ed77968facdb32767dea94bbf
Peter F Schulam <pschulam@gmail.com>
2013-01-31 12:16:55 -0500

    Add pylm.external package to setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name = 'pylm',
     version = '0.1dev',
-    packages = ['pylm',],
+    packages = ['pylm', 'pylm.external'],
     long_description = open('README.md').read(),
     )
