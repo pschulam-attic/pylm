459eb6c653d366d498844e3e91e429563f39ea14
Peter F Schulam <pschulam@gmail.com>
2013-02-18 21:41:41 -0500

    Clean up

--- pylm/common.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def log2(x):
-    return math.log(x, 2)
-
-def log10(x):
-    return math.log(x, 10)
-
--- pylm/jgrams/utils.py
+++
@@ -1,8 +0,0 @@
-def read_token_info(filename):
-    current_seq = []
-    for line in (l.strip() for l in open(filename)):
-        if not line:
-            yield current_seq
-            current_seq = []
-        else:
-            current_seq.append(line.split('\t'))
